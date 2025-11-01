3a0a8be052c17c9de76faefaa4fa3e02b436232b
Tristin Smith <85770698+trisilver25@users.noreply.github.com>
2025-10-31 21:45:13 -0400

    Created home and notfound pages, Installed Lucide React, React Router, Tailwind Merge, and Radix UI. Emptied index css and started defining a custom theme.

--- tsmith/vite.config.ts
+++ tsmith/vite.config.ts
@@ -6,6 +6,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  // set an alias for importing packages and components on files.
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
