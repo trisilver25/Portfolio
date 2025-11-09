91ec5bf787ced17302a1cf2a2581a4019a874ae2
Tristin Smith <85770698+trisilver25@users.noreply.github.com>
2025-11-08 20:49:50 -0500

    Developed the HeroSection component.
    Adjusted text in NavBar.

--- tsmith/src/components/Navbar.tsx
+++ tsmith/src/components/Navbar.tsx
@@ -36,7 +36,7 @@ export const Navbar = () => {
           href="#hero"
         >
           <span className="relative z-10">
-            <span className="text-glow text-foreground"> Tristin Smith </span>{" "}
+            <span className="text-glow text-foreground"> Tristin Smith's </span>{" "}
             Portfolio
           </span>
         </a>
