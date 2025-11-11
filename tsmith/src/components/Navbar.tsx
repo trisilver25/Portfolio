0220924ac61e3d6724726b5a13b95894f0f17bcb
Tristin Smith <85770698+trisilver25@users.noreply.github.com>
2025-11-10 22:02:56 -0500

    Removed backdrop-blur-md from NavBar as it dulled the site on scroll down.
    Removed transparency from bg-background to make the nav bar solid on scroll.

--- tsmith/src/components/Navbar.tsx
+++ tsmith/src/components/Navbar.tsx
@@ -27,7 +27,7 @@ export const Navbar = () => {
     <nav
       className={cn(
         "fixed w-full z-40 transition-all duration-300",
-        isScrolled ? "py-3 bg-background/80 backdrop-blur-md shadow-xs" : "py-5"
+        isScrolled ? "py-3 bg-background shadow-xs" : "py-5"
       )}
     >
       <div className="container flex items-center justify-between">
