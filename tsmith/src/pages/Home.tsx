4368b0495124769a251db1e07cff8c4c4ac614ca
Tristin Smith <85770698+trisilver25@users.noreply.github.com>
2025-11-10 21:39:02 -0500

    Added SkillsSection,
    Changed card-glow to card-hover in index.css

--- tsmith/src/pages/Home.tsx
+++ tsmith/src/pages/Home.tsx
@@ -3,6 +3,7 @@ import { StarBackground } from "../components/StarBackground";
 import { Navbar } from "../components/Navbar";
 import { HeroSection } from "../components/HeroSection";
 import { AboutSection } from "../components/AboutSection";
+import { SkillsSection } from "../components/SkillsSection";
 
 export const Home = () => {
   return (
@@ -22,6 +23,7 @@ export const Home = () => {
       <main>
         <HeroSection />
         <AboutSection />
+        <SkillsSection />
       </main>
 
       {/* Footer */}
